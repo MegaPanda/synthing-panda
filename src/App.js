ea6db1e6a2a2d3716ff8d0cd08e82b4b7a5159cb
Wei-Lun Ma <weilunma@gmail.com>
2021-12-29 18:03:13 +0100

    mobile gesture testing

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import './styles.css'
 function App() {
   return (
     <>
+    {/*
       <div style={{ position: "absolute", bottom: "10px", left: "10px", zIndex: 10, color: "#FAF089", 
           fontFamily: "system-ui", fontSize: "10px", fontWeight: 700
       }}>
         Drag the panda up and<br />
         down to make some noise!
       </div>
+    */}
       <Canvas camera={{ fov: 75, position: [0, 0, 7] }}>
         <color attach="background" args={['#1A202C']} />
         <ambientLight />
